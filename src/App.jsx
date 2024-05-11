94db8b261e4c241b38671d92193adc5d1da98e69
mzn2 <msharyalshrary038@gmail.com>
2024-05-11 15:07:05 +0300

    create pages and foutes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import {BrowserRouter  , Routes, Route} from 'react-router-dom';
+import SignUp from './pages/SignUp';
+import Home from './pages/Home';
+import About from './pages/About';
+import Profile from './pages/Profile';
+import SignIn from './pages/SignIn';
 
 export default function App() {
-  return (
-    <div>
-      hhhhhh
-    </div>
+  return ( <BrowserRouter>
+  <Routes>ddddsf
+  <Route path="/Home" element={ <Home /> } />
+  <Route path="/sign-in" element={ <SignIn/> } />
+  <Route path="/sign-up" element={ <SignUp /> } />
+  <Route path="/about" element={ < About /> } />
+  <Route path="/profile" element={ <Profile /> } />
+  </Routes>
+  </BrowserRouter>
+    
+    
   )
 }
